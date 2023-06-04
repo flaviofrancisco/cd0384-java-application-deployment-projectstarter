b1a48cdfe6247fd43df8808ad5c18fa2bf83646a
Flavio Francisco <flaviofrancis@msn.com>
2023-06-04 20:52:19 +0200

    Bug fix: module-info.java (security)

--- starter/catpoint-parent/security-module/src/main/java/module-info.java
+++ starter/catpoint-parent/security-module/src/main/java/module-info.java
@@ -9,4 +9,5 @@ module security.module {
     exports com.udacity.security.data;
     exports com.udacity.security.application;
     exports com.udacity.security.service;
+    opens com.udacity.security.data to com.google.gson;
 }
